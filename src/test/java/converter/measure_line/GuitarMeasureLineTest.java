3b0c0216f98891bcd933a79ac861cc541f776c59
Adil Hashmi <adilhashmi12345@gmail.com>
2021-02-22 12:32:47 -0500

    Updated measureLineTest, validity1

--- src/test/java/converter/measure_line/GuitarMeasureLineTest.java
+++ src/test/java/converter/measure_line/GuitarMeasureLineTest.java
@@ -7,20 +7,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GuitarMeasureLineTest {
-/*
+
+    /*Testing individual bars*/
     @Test
-    void test1(){
-        String s1 = ("---12---6-4-");
+    void testValidity1(){
+        String s1 = ("---12---6-4-5-8-1-----------2-");
         MeasureLine measureLine = new GuitarMeasureLine(s1, "e", 4);
-        Integer[] expected = {12, 6, 4};
+        Integer[] expected = {12, 6, 4, 5, 8, 1, 2};
         for(int i = 0; i < measureLine.noteList.size(); i++){
             Note note = measureLine.noteList.get(i);
-            assertEquals(note.fret, expected[i]);
+            assertEquals(measureLine.validate().get("Success"), expected);
         }
     }
     @Test
-    void test2(){
-        String s2 = ("-1-------1-1-1-1--");
+    void testValidity2(){
+        String s2 = ("-1-----1-1-1-1--");
         MeasureLine measureLine = new GuitarMeasureLine(s2, "B", 4);
         Integer[] expected = {1, 1, 1, 1, 1};
         for(int i = 0; i < measureLine.noteList.size(); i++){
@@ -29,7 +30,7 @@ public class GuitarMeasureLineTest {
         }
     }
     @Test
-    void test3(){
+    void testValidity3(){
         String s3 = ("-----------------------------28-");
         MeasureLine measureLine = new GuitarMeasureLine(s3, "D", 4);
         Integer[] expected = {28};
@@ -38,6 +39,16 @@ public class GuitarMeasureLineTest {
             assertEquals(note.fret, expected[i]);
         }
     }
-*/
+    @Test
+    void testInvalidity1(){
+        String s4 = ("--------");
+        MeasureLine measureLine = new GuitarMeasureLine(s4, "A", 4);
+        Integer[] expected = {28};
+        for(int i = 0; i < measureLine.noteList.size(); i++){
+            Note note = measureLine.noteList.get(i);
+            assertEquals(note.fret, expected[i]);
+        }
+    }
+
 
 }
