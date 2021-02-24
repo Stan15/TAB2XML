92132f54d1781e5146685f727ac09957b65038ba
Adil Hashmi <adilhashmi12345@gmail.com>
2021-02-24 11:45:04 -0500

    Adding invalidNaming test (unfinished).

--- src/test/java/converter/measure_line/GuitarMeasureLineTest.java
+++ src/test/java/converter/measure_line/GuitarMeasureLineTest.java
@@ -69,5 +69,17 @@ public class GuitarMeasureLineTest {
             assertEquals("false", measureLine.validate().get("success"));
         }
     }
+    @Test
+    void testInvalidNaming(){
+        String[] names = {"x", "z", "v", "q", "w", "u", "y", "i", "o", "p", "X", "Z", "V", "Q", "W", "U", "Y", "I", "O", "P"};
+
+        String s6 = ("|----2--4--7-7----6---|");
+        MeasureLine measureLine = new GuitarMeasureLine(s6, "G", 4);
+        Integer[] expected = {};
+        for(int i = 0; i < names.size(); i++){
+            Note note = measureLine.noteList.get(i);
+            assertEquals("false", measureLine.validate().get("success"));
+        }
+    }
 
 }
