2408741ddc473982962743aeb94b73c6882a39f1
kimboho <kimboho614@gmail.com>
2021-02-25 15:42:42 -0500

    Test file made and change some method from private to public to test

--- src/main/java/converter/measure/GuitarMeasure.java
+++ src/main/java/converter/measure/GuitarMeasure.java
@@ -98,4 +98,7 @@ public class GuitarMeasure extends Measure{
 
         return result;
     }
+
+
+
 }
---
+++ src/test/java/converter/notes/GuitarNotesTest.java
@@ -0,0 +1,33 @@
+package converter.notes;
+
+import converter.GuitarConverter.GuitarNote;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuitarNotesTest {
+
+
+    void makeOctave1(){
+
+    }
+
+    void makeOctave2(){
+
+    }
+
+    void makeKey1(){
+
+    }
+
+    void makeKey2(){
+
+    }
+
+    void makePitch1(){
+
+    }
+
+    void makePitch2(){
+
+    }
+}
